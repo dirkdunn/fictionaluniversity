70f0b14010460c8655462cbb1c0768b5a4ddab0e
Dirk Dunn <dirkdunn1@gmail.com>
2018-10-02 13:42:30 -0400

    Add static assets from theme

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -2,10 +2,24 @@
 <?php
 
     function university_files() {
-        // wp_enqueue_style(stylesheet_name, location_of_file);
-        wp_enqueue_style('style', get_stylesheet_uri());
+        // wp_enqueue_script(str_alias, uri_location, dependencies, str_version, bool_loadatbottom);
+        wp_enqueue_script(
+            'main-js',
+            get_theme_file_uri('/js/scripts-bundled.js'),
+            // Dependencies?
+            NULL,
+            // Version
+            microtime(),
+            // '1.0.0',
+            // Load at bottom of page?
+            true
+        );
+        // wp_enqueue_style(stylesheet_alias, location_of_file, dependencies, str_version, bool_loadatbottom));
+        wp_enqueue_style('style', get_stylesheet_uri(), NULL, microtime());
+        wp_enqueue_style('font-awesome-css', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     }
 
-    /*add_action(at_which_moment_to_call, function_name)*/
+    // add_action(at_which_moment_to_call, function_name)
     add_action('wp_enqueue_scripts','university_files');
 ?>
